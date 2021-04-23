282bb778e3d3607fa8a63f9ccf90a673f0e1ce9c
AlexMuzzy <alexmuzzy33@gmail.com>
2021-04-23 23:55:29 +0100

    added gazebo models and updated automatic control.

--- auto_twist_control/auto_twist_control/control.py
+++ auto_twist_control/auto_twist_control/control.py
@@ -6,7 +6,6 @@ import cv2
 import numpy as np
 import rclpy
 from cv_bridge import CvBridge, CvBridgeError
-from
 
 
 class RobotControl(Node):
@@ -20,9 +19,17 @@ class RobotControl(Node):
         self.geometry_move_pub_topic: str = '/cmd_vel'
         self.default_angular_speed: int = 0.5
         self.default_linear_speed: int = 0.5
-        self.show_image: bool = False
+        self.show_image: bool = True
         self.bridge: object = CvBridge()
         self.logger = self.get_logger()
+        self.angular_strengths = {
+            'far-left': 1.0,
+            'left': 0.5,
+            'straight': 0,
+            'right': -0.5,
+            'far-right': -1.0
+
+        }
 
         self.logger.info('Initialising subscription for %s' %
                          self.image_depth_sub_topic)
@@ -46,12 +53,11 @@ class RobotControl(Node):
             self.display_cv_image(cv_image, 'Optical Image')
 
     def depth_image_callback(self, message: Image) -> None:
-        cv_image = self.convert_ros_imgmsg_to_cv_frame(
-            message, colour_image=False)
+        cv_image = self.convert_ros_imgmsg_to_cv_frame(message)
+        test = self.convert_depth_to_usable_np_frame(cv_image)
 
-        np_depth_frame =
         if self.show_image:
-            self.display_cv_image(cv_image, 'Depth Image')
+            self.display_cv_image(test, 'Depth Image')
 
         self.logger.info('Average Depth image value: %d' %
                          np.average(cv_image).astype(np.uint8))
@@ -67,16 +73,21 @@ class RobotControl(Node):
         cv2.imshow(window_title, image)
         cv2.waitKey(1)
 
-    def convert_ros_imgmsg_to_cv_frame(self, image, colour_image=True):
+    def convert_depth_to_usable_np_frame(self, frame: np.ndarray):
+        # equate unusable values to 0.
+        frame[np.isinf(frame)] = 0
+        # Normalise frame.
+        return (frame - frame.min()) / (frame.max() - frame.min())
+
+    def convert_ros_imgmsg_to_cv_frame(self, image, depth_image=False):
         try:
-            cv_image = self.bridge.imgmsg_to_cv2(image)
-            if colour_image:
-                return cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
+            if depth_image:
+                return self.bridge.imgmsg_to_cv2(image)
             else:
-                return cv_image
+                return cv2.cvtColor(self.bridge.imgmsg_to_cv2(image), cv2.COLOR_BGR2RGB)
 
         except CvBridgeError as e:
-            self.get_logger().error(e)
+            raise e
 
 
 def main(args=None):
@@ -89,7 +100,9 @@ def main(args=None):
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    robot_control.control_publisher.publish(Twist())
+    stop_movement = Twist()
+    stop_movement.linear.x = 0
+    robot_control.control_publisher.publish(stop_movement)
     robot_control.destroy_node()
     rclpy.shutdown()
     cv2.destroyAllWindows()
