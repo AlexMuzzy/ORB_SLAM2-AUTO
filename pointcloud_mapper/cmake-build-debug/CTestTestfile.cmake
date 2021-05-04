76506df6b76c757b7604bd4852f66afe9babbd54
AlexMuzzy <alexmuzzy33@gmail.com>
2021-05-04 13:56:45 +0100

    Removed build files from git repo.

--- pointcloud_mapper/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alexmuzzy/ros/ws/src/pointcloud_mapper
-# Build directory: /home/alexmuzzy/ros/ws/src/pointcloud_mapper/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
